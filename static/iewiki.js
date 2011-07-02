3f1f6e16ffff1cd0814d8fc5d6777c3e8bac24cb
poul.staugaard <poul.staugaard@902308ec-1e26-11df-93fd-f15358bbf208>
2011-07-02 09:35:15 +0000

    availableTemplates() needs to be a global function, used also in CreateNewPage dialog
    git-svn-id: http://giewiki.googlecode.com/svn/trunk@305 902308ec-1e26-11df-93fd-f15358bbf208

--- static/iewiki.js
+++ static/iewiki.js
@@ -1,7 +1,7 @@
 /* this:	iewiki.js
    by:  	Poul Staugaard
    URL: 	http://code.google.com/p/giewiki
-   version:	1.12.2
+   version:	1.12.3
 
 Giewiki is based on TiddlyWiki created by Jeremy Ruston (and others)
 
@@ -8396,6 +8396,12 @@ function InsertTiddlerText(title, text, message) {
 		clearMessage();
 		wikify(message.htmlDecode(), getMessageDiv());
 	}
+}
+
+function availableTemplates() {
+	var tl = http.getTemplates({ template: forms.PageProperties && forms.PageProperties.tags && forms.PageProperties.tags.indexOf('template') >= 0 ? forms.PageProperties.title : "" } );
+	if (tl.Success)
+		return tl.templates.join('|');
 }
 
 config.macros.confirm_replace = {
