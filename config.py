fab1521a9048174ce1154b83c527e99a4caa961b
poul.staugaard <poul.staugaard@902308ec-1e26-11df-93fd-f15358bbf208>
2011-07-04 12:21:59 +0000

    Release 1.12.3
    git-svn-id: http://giewiki.googlecode.com/svn/trunk@309 902308ec-1e26-11df-93fd-f15358bbf208

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # this:	Config.py
 # by:	Poul Staugaard (poul(dot)staugaard(at)gmail...)
 # URL:	http://code.google.com/p/giewiki
-# ver.:	1.12.2
+# ver.:	1.12.3
 
 import cgi
 import codecs
@@ -68,7 +68,7 @@ updateTemplate\n\
 getTemplates'
 
 jsProlog = '\
-var giewikiVersion = { title: "giewiki", major: 1, minor: 12, revision: 2, date: new Date("June 25, 2011"), extensions: {} };\n\
+var giewikiVersion = { title: "giewiki", major: 1, minor: 12, revision: 3, date: new Date("July 4, 2011"), extensions: {} };\n\
 http = {\n\
   _methods: [],\n\
   _addMethod: function(m) { this[m] = new Function("a","return HttpGet(a,\'" + m + "\')"); }\n\
